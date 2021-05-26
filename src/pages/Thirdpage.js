e8569cae1ed69fd3d5302ee6d7a9eca8c589fe86
maoxin53 <maoxin53@gmail.com>
2021-05-27 00:10:32 +0800

    changed placeholder for school

--- src/pages/Thirdpage.js
+++ src/pages/Thirdpage.js
@@ -1,39 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Thirdpage = () => {
-    return (
-        console.log('thirdpage reached'),
-        <div className='pagefiller'>
-            <h2 className='header'>
-                Register an Account
-            </h2>
+  return (
+    console.log("thirdpage reached"),
+    (
+      <div className="pagefiller">
+        <h2 className="header">Register an Account</h2>
 
-            <h3 className='container'>
-
-            <form className='add-form'>
-                <div className='form-control'>
-                    <label>Full Name</label>
-                    <input type='name' placeholder='First and Last Name' ></input>
-                </div>
-                <div className='form-control'>
-                    <label>School</label>
-                    <input type='school' placeholder='School'></input>
-                </div>
-                <div className='form-control'>
-                    <label>Email</label>
-                    <input type='email' placeholder='example@gmail.com'></input>
-                </div>
-                <div className='form-control'>
-                    <label>Password</label>
-                    <input type='password' placeholder='' ></input>
-                </div>
-            </form>
-
-            </h3>
-
-
-        </div>
+        <h3 className="container">
+          <form className="add-form">
+            <div className="form-control">
+              <label>Full Name</label>
+              <input type="name" placeholder="First and Last Name"></input>
+            </div>
+            <div className="form-control">
+              <label>School</label>
+              <input
+                type="school"
+                placeholder="National University of Singapore"
+              ></input>
+            </div>
+            <div className="form-control">
+              <label>Email</label>
+              <input type="email" placeholder="example@gmail.com"></input>
+            </div>
+            <div className="form-control">
+              <label>Password</label>
+              <input type="password" placeholder=""></input>
+            </div>
+          </form>
+        </h3>
+      </div>
     )
-}
+  );
+};
 
-export default Thirdpage
+export default Thirdpage;
