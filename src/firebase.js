ba187d9b3b544d4dca933c2e239da916f27d52c2
pollackroe <84116995+pollackroe@users.noreply.github.com>
2021-06-09 17:40:50 +0800

    Revert "updated submit submissions according to new change in users field in db"
    This reverts commit b28ef291f906f9331dfd860663577624c9a849ba.

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,27 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/storage"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyD0mnZlosl4Ah_Wj1dDMrNdWWuitnJ2c6s",
-  authDomain: "auth-production-30b35.firebaseapp.com",
-  projectId: "auth-production-30b35",
-  storageBucket: "auth-production-30b35.appspot.com",
-  messagingSenderId: "563968072455",
-  appId: "1:563968072455:web:5b7f2b12a411fa8b2f03a2",
-  measurementId: "G-JLBV0X4VS0",
-});
+    apiKey: 'AIzaSyD0mnZlosl4Ah_Wj1dDMrNdWWuitnJ2c6s',
+    authDomain: 'auth-production-30b35.firebaseapp.com',
+    projectId: 'auth-production-30b35',
+    storageBucket: 'auth-production-30b35.appspot.com',
+    messagingSenderId: '563968072455',
+    appId: '1:563968072455:web:5b7f2b12a411fa8b2f03a2',
+    measurementId: 'G-JLBV0X4VS0',
+})
 
-export const auth = app.auth();
-export const db = app.firestore();
-export const storageRef = app.storage().ref();
-export default app;
+export const auth = app.auth()
+export const db = app.firestore()
+export const storageRef = app.storage().ref()
+export default app
+
+
+
+
+
+
+
+
