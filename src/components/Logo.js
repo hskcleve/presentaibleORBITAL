76efddad562021243d5db9cb47484bfc8a74793b
maoxin53 <maoxin53@gmail.com>
2021-06-12 14:44:54 +0800

    Change color of Logo
    PresentAIble now has orange as the color instead of white.
    other color suggestions: goldenRod, darkGoldenRod

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Logo = ({fontSize, marginTop}) => {
-    const AIstyling={color:'GhostWhite'}
-    return (
-        <div className='header' style={{fontSize:fontSize, marginTop:marginTop}}>
-        Present<h3 style={AIstyling}>AI</h3>ble
-        </div>
-    )
-}
+const Logo = ({ fontSize, marginTop }) => {
+  const AIstyling = { color: "orange" };
+  return (
+    <div
+      className="header"
+      style={{ fontSize: fontSize, marginTop: marginTop }}
+    >
+      Present<h3 style={AIstyling}>AI</h3>ble
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
