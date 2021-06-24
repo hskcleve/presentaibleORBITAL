12670ec04ecf900e906f92b1160e63e81a350d40
maoxin53 <maoxin53@gmail.com>
2021-06-25 03:14:25 +0800

    submit comment integrates with api
    useEffect listens for changes in user submitting a comment, then api calls to backend is made, then update post via firestore update and increments.

--- src/components/SubmitComment.js
+++ src/components/SubmitComment.js
@@ -1,47 +1,97 @@
-import { useState } from 'react';
-import { db } from '../firebase';
+import React, { useState, useEffect, useRef } from "react";
+import { db } from "../firebase";
 import firebase from "firebase";
 
 const SubmitComment = () => {
-    const user = firebase.auth().currentUser;
-    const [comment, setComment] = useState('');
-    const commentUID = String(window.location.pathname.substring(10, window.location.pathname.length));
+  const user = firebase.auth().currentUser;
+  const [comment, setComment] = useState("");
+  const postUID = String(
+    window.location.pathname.substring(10, window.location.pathname.length)
+  );
+  const [updated, setUpdated] = useState(false);
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": " application/json" },
+    body: JSON.stringify({ content: comment }),
+  };
+  const uid = user.uid;
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        db.collection('comments').add({
-            PostUID: commentUID,
-            author: user.displayName,
-            content: comment,
+  useEffect(() => {
+    console.log(comment);
+    if (updated) {
+      fetch("/post", requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          //todo -----
+          //update database
+          db.collection("submissions")
+            .doc(postUID)
+            .update({
+              [data.message]: firebase.firestore.FieldValue.increment(1),
+              totalFeedbacks: firebase.firestore.FieldValue.increment(1),
+            });
+          //cause a reload of the page
+          console.log(data);
         });
-        setComment("");
+      console.log("updated has a value of ", updated);
+      //add comment to db
+      db.collection("comments").add({
+        PostUID: postUID,
+        author: user.displayName,
+        content: comment,
+      });
+      setUpdated(false);
+      setComment("");
     }
-  
-    const handleCommentAdd = (event) => {
-        setComment(event.target.value)
+    //add rating of this comment to submissions db
+  }, [updated]);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (comment != "") {
+      setUpdated(true);
+    } else {
+      alert("Leave a feedback!");
     }
+  };
 
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                    Add a comment:
-                    <br></br>
-                    <textarea id="comment" name="comment"
-                        type='text'
-                        placeholder=' ...'
-                        value={comment}
-                        onChange={handleCommentAdd} 
-                        style={{minHeight:120, maxHeight:120, minWidth:435, maxWidth:435,
-                            fontFamily: "Helvetica Neue", fontSize:15}}
-                            />
-            
-                <div className='center'>
-                    <input className='btn' type='submit' value='Add Comment' />
+  const handleCommentAdd = (event) => {
+    setComment(event.target.value);
+  };
 
-                </div>
-            </form>
+  return (
+    <div>
+      <form>
+        Leave a feedback:
+        <br></br>
+        <textarea
+          id="comment"
+          name="comment"
+          type="text"
+          placeholder=" ..."
+          value={comment}
+          onChange={handleCommentAdd}
+          style={{
+            minHeight: 120,
+            maxHeight: 120,
+            minWidth: 435,
+            maxWidth: 435,
+            fontFamily: "Helvetica Neue",
+            fontSize: 15,
+          }}
+        />
+        <div className="center">
+          <button
+            style={{ backgroundColor: "goldenrod" }}
+            onClick={onSubmit}
+            className="btn"
+          >
+            Feedback
+          </button>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 
-export default SubmitComment
+export default SubmitComment;
