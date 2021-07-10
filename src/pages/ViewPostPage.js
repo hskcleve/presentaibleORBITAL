dbb0a0315b810cace562cbb8ef6d4356fabf8c83
maoxin53 <maoxin53@gmail.com>
2021-07-10 15:07:35 +0800

    Comments show their rating
    changed frontend + firebase

--- src/pages/ViewPostPage.js
+++ src/pages/ViewPostPage.js
@@ -9,9 +9,7 @@ import firebase from "firebase";
 import EditSubmission from "../components/EditSubmission";
 
 const ViewPostPage = () => {
-  const PostUID = String(
-    window.location.pathname.substring(18, window.location.pathname.length)
-  );
+  const PostUID = String(window.location.pathname.substring(18, window.location.pathname.length));
   const [postContent, setPostContent] = useState("");
   const [postAuthor, setPostAuthor] = useState("");
   const [postComments, setPostComments] = useState([]);
@@ -57,9 +55,7 @@ const ViewPostPage = () => {
         setPostAuthor(data.author);
         setFilename(data.attachedFileName);
         setPostTitle(data.title);
-        setFeedback(
-          data.totalFeedbacks != 0 && data.totalFeedbacks != undefined
-        );
+        setFeedback(data.totalFeedbacks != 0 && data.totalFeedbacks != undefined);
         setGoodFeedbacks(data.good);
         setBadFeedbacks(data.bad);
         setNeutralFeedbacks(data.neutral);
@@ -76,51 +72,80 @@ const ViewPostPage = () => {
         const tempCommentArray = [];
         querySnapshot.forEach((doc) => {
           const data = doc.data();
-          tempCommentArray.push([data.author, data.content]);
+          tempCommentArray.push([data.author, data.content, data.rating]);
         });
         setPostComments(tempCommentArray);
       });
   };
 
   const goBack = () => {
-    history.push("/" + origin)
-  }
+    history.push("/" + origin);
+  };
 
   function renderEditSubmission() {
     if (postAuthor !== user.displayName) {
-      return
+      return;
     }
-    console.log('btn clicked')
+    console.log("btn clicked");
     return (
-      <div className='center'>
+      <div className="center">
         <EditSubmission
-        postUID={PostUID}
-        previousTitle={postTitle}
-        previousContent={postContent}
-        >
-        </EditSubmission>
-        </div>
+          postUID={PostUID}
+          previousTitle={postTitle}
+          previousContent={postContent}
+        ></EditSubmission>
+      </div>
     );
+  }
+
+  function commentColor(ratings) {
+    console.log("comment color", ratings);
+    if (ratings === "good") {
+      return "#e7f8d1";
+    } else if (ratings === "bad") {
+      return "#ffc2c3";
+    } else {
+      return "#f2e2ce";
+    }
+  }
+
+  function loadCommentSection() {
+    return postComments.map((comments) => (
+      <div
+        className="submission"
+        style={{ fontSize: 12, backgroundColor: commentColor(comments[2]) }}
+      >
+        <h3>Comment by: {comments[0]}</h3>
+        <p>{comments[1]}</p>
+      </div>
+    ));
   }
 
   return (
     <div>
       <Navbar />
       <br></br>
-      <button className='btn' onClick={()=>{goBack()}}>← Back to {origin}</button>
-      <div className="viewpost-info-wrapper" style={{display:'flex', alignItems:'center'}}>
-      <div style={{display:'flex', flexDirection:'column'}}>
+      <button
+        className="btn"
+        onClick={() => {
+          goBack();
+        }}
+      >
+        ← Back to {origin}
+      </button>
+      <div className="viewpost-info-wrapper" style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <h1>{postTitle}</h1>
           <h3>by {postAuthor}</h3>
           <h6>PostUID: {PostUID}</h6>
-          </div>
+        </div>
         <PlayAudioFromURL downloadURL={downloadURL} />
         <Feedback
           feedback={feedback}
           badFeedbacks={badFeedbacks}
           goodFeedbacks={goodFeedbacks}
-          neutral={neutralFeedbacks}>
-          </Feedback>
+          neutral={neutralFeedbacks}
+        ></Feedback>
       </div>
       <div className="container">
         <strong>Script:</strong> {postContent}
@@ -138,12 +163,7 @@ const ViewPostPage = () => {
         >
           refresh comments
         </button>
-        {postComments.map((comments) => (
-          <div className="submission" style={{ fontSize: 12 }}>
-            <h3>Comment by: {comments[0]}</h3>
-            <p>{comments[1]}</p>
-          </div>
-        ))}
+        {loadCommentSection()}
         <SubmitComment
           badFeedbacks={badFeedbacks}
           goodFeedbacks={goodFeedbacks}
