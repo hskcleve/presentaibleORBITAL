2879534bda77f3c0945d23aee166dd1823dbb5af
maoxin53 <maoxin53@gmail.com>
2021-07-12 23:50:07 +0800

    new alternate version of brains
    pleasework2.json is the default 20k iterations

--- src/testBrain.js
+++ src/testBrain.js
@@ -1,21 +1,20 @@
 const brain = require("brain.js");
+const fs = require("fs");
 const trainingDataJson = require("./mxData.json"); //change to prefered training data file
 const network = new brain.recurrent.LSTM();
 const trainingData = trainingDataJson.map((item) => ({
   input: item.input,
-  output: item.output
+  output: item.output,
 }));
 const alternatedData = [];
-for (var a = 0; a < (trainingData.length/2)-10; a++) {
-    alternatedData.push(trainingData[a]);
-    alternatedData.push(trainingData[trainingData.length-a-1]);
+for (var a = 0; a < trainingData.length / 2 - 10; a++) {
+  alternatedData.push(trainingData[a]);
+  alternatedData.push(trainingData[trainingData.length - a - 1]);
 }
 console.log(alternatedData);
-network.train(alternatedData, {
-   iterations: 1000,
-  });
+console.log("new one");
+network.train(alternatedData);
 
 const json = network.toJSON();
 const data = JSON.stringify(json);
-fs.writeFileSync("pleaseWork.json", data); //rewrite the name of trained model of brainjs
-
+fs.writeFileSync("pleaseWork2.json", data); //rewrite the name of trained model of brainjs
