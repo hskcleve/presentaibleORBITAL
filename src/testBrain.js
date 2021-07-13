705a2de467c323daa277f30be09ab21f096ddf80
pollackroe <84116995+pollackroe@users.noreply.github.com>
2021-07-13 15:03:03 +0800

    trained data 3
    works well with 2000 iterations.

--- src/testBrain.js
+++ src/testBrain.js
@@ -7,14 +7,18 @@ const trainingData = trainingDataJson.map((item) => ({
   output: item.output,
 }));
 const alternatedData = [];
-for (var a = 0; a < trainingData.length / 2 - 10; a++) {
+for (var a = 0; a < trainingData.length/2; a++) {
   alternatedData.push(trainingData[a]);
   alternatedData.push(trainingData[trainingData.length - a - 1]);
 }
 console.log(alternatedData);
 console.log("new one");
-network.train(alternatedData);
+network.train(alternatedData, {
+  iterations: 2000, // the maximum times to iterate the training data --> number greater than 0
+  log: true, // true to use console.log, when a function is supplied it is used --> Either true or a function
+  logPeriod: 1, // iterations between logging out --> number greater than 0
+});
 
 const json = network.toJSON();
 const data = JSON.stringify(json);
-fs.writeFileSync("pleaseWork2.json", data); //rewrite the name of trained model of brainjs
+fs.writeFileSync("pleaseWork3.json", data); //rewrite the name of trained model of brainjs
