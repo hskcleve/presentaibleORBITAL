371d7eb45b738fd417ebf49298f25b0f09781331
maoxin53 <maoxin53@gmail.com>
2021-07-13 18:29:14 +0800

    frontend option loading
    the rendering of options of undeleted modules only is now checked by dashboard

--- src/components/TutorSubmissions.js
+++ src/components/TutorSubmissions.js
@@ -6,9 +6,9 @@ const TutorSubmission = (props) => {
   console.log(moduleList.map((mod) => console.log(mod.className)));
   return (
     <div>
-      <h2 style={{color:'ghostwhite'}}>My Classes</h2>
-      <p style={{color:'antiqueWhite'}}>Tutor views students' submissions by class</p>
-      <ViewSubmissionByClass moduleCode={"Orbital"}></ViewSubmissionByClass>
+      <h2 style={{ color: "ghostwhite" }}>My Classes</h2>
+      <p style={{ color: "antiqueWhite" }}>Tutor views students' submissions by class</p>
+      <ViewSubmissionByClass modules={moduleList}></ViewSubmissionByClass>
     </div>
   );
 };
